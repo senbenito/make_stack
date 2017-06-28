4565b4b36b563a7a18f8950040d3f035a8b5125d
Shannon Rivers <shannon.rivers@gmail.com>
2017-06-28 10:01:50 -0600

    pass all tests

--- stack.js
+++ stack.js
@@ -1,5 +1,42 @@
 function Stack(){
-  this.items = new Array();
+    this.items = new Array();
+
+    return {
+      empty:()=>{
+        if(this.items.length === 0){
+          return true;
+        } else {
+          return false;
+        }
+      },
+
+      push:(e)=>{
+        this.items.push(e);
+        console.log(this.items);
+        return e;
+      },
+
+      peek:()=>{
+        return this.items[0];
+      },
+
+      pop:()=>{
+        return this.items.pop();
+      },
+
+      search:(e)=>{
+        if(this.items.indexOf(e)===-1){
+          return 0.1;
+        } else {
+          return this.items.indexOf(e);
+        }
+      },
+
+    };
+
+
 }
+
+
 
 module.exports = Stack;
